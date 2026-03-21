3b02775e63be6b7a2b45ccd4a6a3d744ca5bec43
Astik-2002 <astik.srivastava@research.iiit.ac.in>
2026-03-21 12:59:03 +0530

    added absolute error, relative error and drift computation in orb slam example

--- examples/orb_slam/src/main.rs
+++ examples/orb_slam/src/main.rs
@@ -361,6 +361,22 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
         // Trajectory.
         trajectory.push(trajectory_point_from_pose(&result.pose_world_to_cam));
+        let p_est = trajectory_point_from_pose(&result.pose_world_to_cam);
+
+        if let Some(gt_pose) = associate_gt(sample.timestamp_sec, ground_truth) {
+
+            est_positions.push(Vector3::new(
+                p_est[0] as f64,
+                p_est[1] as f64,
+                p_est[2] as f64
+            ));
+
+            gt_positions.push(Vector3::new(
+                gt_pose.tx,
+                gt_pose.ty,
+                gt_pose.tz
+            ));
+        }
 
         // Rerun logging.
         #[cfg(feature = "viz")]
